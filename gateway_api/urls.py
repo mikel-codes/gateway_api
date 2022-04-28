02ea7648039212a048f30899c3697f74a5c4f2e2
smeez <gatez@yahoo.com>
2022-04-28 10:31:16 +0100

    Implemented restapi with api/ slash

--- gateway_api/urls.py
+++ gateway_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('router/', include('router.urls'))
+    path('api/router/', include('router.urls'))
 ]
