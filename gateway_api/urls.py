8c7c3d9c3730f4778216d2e306eaeb9fe9774cee
smeez <gatez@yahoo.com>
2022-04-29 09:08:15 +0100

    Fix namespacing required app name

--- gateway_api/urls.py
+++ gateway_api/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/router/', include('router.urls', namespace="router_api"))
+    path('api/router/', include(('router.urls','router'), namespace="router_api"))
 ]
